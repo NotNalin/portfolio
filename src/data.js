c580b23a9f7782dd672cc340bf44c4427ddda1c4
Nalin <79359909+NotNalin@users.noreply.github.com>
2024-04-23 04:30:52 +0530

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "rtadubai",
+    subtitle: "An Unoffical API for RTA Dubai",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./1.png",
+    link: "https://github.com/NotNalin/rtadubai",
+  },
+  {
+    title: "SHJ-Polize",
+    subtitle: "Multipurpose discord bot",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./2.png",
+    link: "https://shj.stooby.tech",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Python",
+  "Discord.py",
+  "Pycord",
+  "C"
+];
