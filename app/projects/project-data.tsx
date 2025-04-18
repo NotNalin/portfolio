2d9a5f6544ede73788a009dfe696baa34e9023ec
Nalin <79359909+NotNalin@users.noreply.github.com>
2025-04-18 10:04:26 +0530

    Add projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,6 +7,18 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "go.notnal.in",
+    year: 2025,
+    description: "A URL shortener with multiple features",
+    url: "https://go.notnal.in",
+  },
+  {
+    title: "Google Meet dark mode extension",
+    year: 2025,
+    description: "A dark mode extension for Google Meet",
+    url: "https://github.com/NotNalin/google-meet-dark-mode-extension"
+  },
+  {
     title: "SHJ-Polize",
     year: 2022,
     description: "Multi-purpose Discord bot",
